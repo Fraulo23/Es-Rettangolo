cdb3566491402793ee95eecb524bd7f5d2dad3ef
Fraulo23 <fraulinifederico@gmail.com>
2024-01-17 16:57:19 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_Rettangolo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float l1=float.Parse(Console.ReadLine());
+            float l2=float.Parse(Console.ReadLine());
+            Rettangolo r=new Rettangolo(l1,l2);
+            Console.WriteLine("Area e perimetro rettangolo");
+            Console.WriteLine(r.Area());
+            Console.WriteLine(r.Perimetro());
+            float lq=float.Parse(Console.ReadLine());
+            Rettangolo q=new Rettangolo(lq,lq);
+            Console.WriteLine("Area e perimetro quadrato");
+            Console.WriteLine(q.Area());
+            Console.WriteLine(q.Perimetro());
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Rettangolo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Es_Rettangolo
+{
+    internal class Rettangolo
+    {
+        public float L1 { get; set; }
+        public float L2 { get; set; }
+        public Rettangolo(float l1, float l2)
+        {
+            L1 = l1;
+            L2 = l2;
+        }
+        public float Area()
+        {
+            return (L1 * L2);
+        }
+        public float Perimetro() {
+            return (L1 + L2) * 2;
+        }
+
+    }
+}
